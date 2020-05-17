ba3b5c719f6b3ea672aff5bbab0de9a44579a8a3
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-17 18:27:26 +0430

    added webconfigs,frontend page and urls

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends React {
+  render() {
+    return <h1>Hi</h1>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
