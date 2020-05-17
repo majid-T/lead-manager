fc2f80091e3ac52df7446473ac296bf946a65fbc
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-17 18:54:51 +0430

    Added Header component

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Header from "./layout/Header";
 
 class App extends Component {
   render() {
-    return <h1>Hi</h1>;
+    return <Header />;
   }
 }
 
