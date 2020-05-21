d341bcb9318ff204e0884faa1d3707c2baa978ff
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-05-21 23:54:47 +0430

    Adding msg to state for using in alerts

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { createMessage } from "./messages";
 import { GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS } from "./types";
 
 //GET all leads action
@@ -19,6 +20,7 @@ export const deleteLead = (id) => (dispatch) => {
   axios
     .delete(`/api/leads/${id}/`)
     .then((res) => {
+      dispatch(createMessage({ deleteLead: "Lead Deleted" }));
       dispatch({
         type: DELETE_LEAD,
         payload: id,
@@ -32,6 +34,8 @@ export const addLead = (lead) => (dispatch) => {
   axios
     .post(`/api/leads/`, lead)
     .then((res) => {
+      dispatch(createMessage({ addLead: "Lead Added" }));
+
       dispatch({
         type: ADD_LEAD,
         payload: res.data,
--- leadmanager/frontend/src/actions/types.js
+++ leadmanager/frontend/src/actions/types.js
@@ -2,3 +2,4 @@ export const GET_LEADS = "GET_LEADS";
 export const DELETE_LEAD = "DELETE_LEADS";
 export const ADD_LEAD = "ADD_LEADS";
 export const GET_ERRORS = "GET_ERRORS";
+export const CREATE_MESSAGE = "CREATE_MESSAGE";
